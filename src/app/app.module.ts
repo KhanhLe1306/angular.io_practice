78de81fc3faf7777c6b1afb3cb25ac968da1f783
Khanh Le <khanhle@unomaha.edu>
2022-02-26 21:29:56 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertComponent } from './product-alert/product-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProductListComponent,
+    ProductAlertComponent
   ],
   imports: [
     BrowserModule,
